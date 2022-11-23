158a5c8f8774313aa721ad7d78019a854a482b8d
Nurmuhammad <abdulaxatov3@gmail.com>
2022-11-23 20:20:01 +0500

    doc write qowldi

--- app.js
+++ app.js
@@ -19,3 +19,12 @@ console.log("Kun: " + Math.round((now - day) / 1000 / 60 / 60 / 24) + ";");
 console.log("Soat: " + Math.round((now - day) / 1000 / 60 / 60) + ";");
 console.log("Sekund: " + Math.round(now - day) + ";");
 console.log("bo'ldi, shuncha payt nima qildingiz?");
+
+document.write("Siz tug'ilganingizga");
+document.write(
+  "Yil: " + Math.round((now - day) / 1000 / 60 / 60 / 24 / 365.25) + ";"
+);
+document.write("Kun: " + Math.round((now - day) / 1000 / 60 / 60 / 24) + ";");
+document.write("Soat: " + Math.round((now - day) / 1000 / 60 / 60) + ";");
+document.write("Sekund: " + Math.round(now - day) + ";");
+document.write("bo'ldi, shuncha payt nima qildingiz?");
